a0183a49332376f8d78e0918bd2003571fa64e44
gchug <105833137+gchug@users.noreply.github.com>
2025-08-01 15:25:40 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -29,7 +29,7 @@ for message in st.session_state.messages:
 # Accept user input
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
-    with st.chat_message("user"):
+with st.chat_message("user"):
         st.markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
